b0b4c50cef9c4cf6bd67c99e5e0ca1a1e21927a4
Rakha-arch <miftahuzahran@gmail.com>
2025-06-09 17:36:27 +0700

    membuat kalimat selamat belajar di ti umy

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -5,15 +5,15 @@ int main(){
     try {
         cout << "selamat belajar di Ti umy" << endl;
         throw 0,5; //melempar sebuah interger maka
-        cout << "pernyataan tidak akan di eksekusi" << endl;
+       
     }
     catch (int a){
         //blok ini akan di eksekusi
-        cout << "pengecualian akan di eksekusi" << endl;
+        
     }
     catch (...) {
         /*jika selain integer maka blok ini akan di eksekusi*/
-        cout << "default pengecualian dieksekusi" << endl;
+       
     }
     return 0;
 }  
