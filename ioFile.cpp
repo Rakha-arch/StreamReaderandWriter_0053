29b2bcb64d4271b33b6b7b7ab016a946a4f3e033
Rakha-arch <miftahuzahran@gmail.com>
2025-06-09 11:13:42 +0700

    menampilkan

--- ioFile.cpp
+++ ioFile.cpp
@@ -39,7 +39,7 @@ int main(){
         while (getline(infile,baris))
         {
             //dan tampilka disini
-           
+            cout << baris << '\n';
         }
         //tutup file tersebut setelah selesai
         
