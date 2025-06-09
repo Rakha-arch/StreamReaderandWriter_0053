38e078b19fea0ba61b23e562befceb47cdc4e3eb
Rakha-arch <miftahuzahran@gmail.com>
2025-06-09 17:37:40 +0700

    melempar sebuah integer maka pernyataan tidak akan di eksekusi

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -5,7 +5,7 @@ int main(){
     try {
         cout << "selamat belajar di Ti umy" << endl;
         throw 0,5; //melempar sebuah interger maka
-       
+        cout << "pernyataan tidak akan di eksekusi" << endl;
     }
     catch (int a){
         //blok ini akan di eksekusi
